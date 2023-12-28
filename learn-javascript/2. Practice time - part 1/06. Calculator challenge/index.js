f0d690c7990b1aeebf18e6a3cc04d8376c541037
dcnadar <dcnadar0@gmail.com>
2023-12-29 02:01:39 +0530

    If else blackjack file

--- learn-javascript/2. Practice time - part 1/06. Calculator challenge/index.js
+++ learn-javascript/2. Practice time - part 1/06. Calculator challenge/index.js
@@ -1,4 +1,4 @@
-let num1 = 0
+let num1 = 8
 let num2 = 5
 let res =0
 document.getElementById("num1-el").textContent = num1
---
+++ learn-javascript/3. Build a Blackjack game/01. Let's build a Blackjack game!/index.js
@@ -0,0 +1,15 @@
+// 1. Create two variables, firstCard and secondCard.
+// Set their values to a random number between 2-11
+let firstCard=5;
+let secondCard=9;
+// 11 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard+secondCard
+if (sum<21) {
+    console.log("Do you want another car dto draw");
+} else if(sum===21){
+    console.log("You've got blackjack");
+} else{
+    console.log("You Lost");
+}
+
+// console.log(sum);
