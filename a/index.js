5f5332aa42c87dd7501efc58d4ac025115dc21f8
Deepak Chaudhary <144508555+dcnadar@users.noreply.github.com>
2023-12-28 22:42:45 +0530

    Delete a directory

--- a/index.js
+++
@@ -1,8 +0,0 @@
-// function msg(){
-//     console.log("Welcome Deepak")
-// }
-let gM=document.getElementById('greet-msg');
-let msg = "Welcome to this course, ";
-let nam = "Deepak"
-gM.innerText=msg+nam;
-gM.innerText+="✌🏼"
