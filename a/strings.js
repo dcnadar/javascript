9631b51892fbb6daa89cbb1a381a145304498592
dcnadar <dcnadar0@gmail.com>
2023-12-28 22:14:05 +0530

    new files for practicing

---
+++ a/strings.js
@@ -0,0 +1,16 @@
+// Example 1
+let num=4 ;
+let str="Hey this will be a string ";
+let st=str+num
+console.log(st) // resut is a string
+
+// Example 2
+let num2=4
+let str2="10"
+console.log(str2+num2); //result is a string
+
+//Example 3
+console.log(4+2); //number
+console.log("4"+"2"); //string
+console.log("4"+2+2); //string
+console.log(4*4+"2"); //string but first it add the two numbers
---
+++ learn-javascript/2. Practice time - part 1/04. Strings and numbers/index.js
@@ -0,0 +1,7 @@
+// Try to predict what each of the lines will log out
+console.log("2" + 2) // string
+console.log(11 + 7) // number
+console.log(6 + "5") // string
+console.log("My points: " + 5 + 9) // string
+console.log(2 + 2) // integer
+console.log("11" + "14") // string 
